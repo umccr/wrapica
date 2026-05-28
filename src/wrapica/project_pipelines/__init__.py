546fcc2aa182f902e4e4f7132b093c60538bb10b
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-05-28 20:02:32 +1000

    Import globals before submodule imports

--- src/wrapica/project_pipelines/__init__.py
+++ src/wrapica/project_pipelines/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from pathlib import Path
+
 # Standard imports
+from pathlib import Path
 from typing import Union
 
 # Libica objects
@@ -31,6 +32,19 @@ from libica.openapi.v3.models import (
     SearchMatchingActivationCodesForCwlAnalysis,
     SearchMatchingActivationCodesForNextflowAnalysis
 )
+
+# Literal / Types
+AnalysisType = Union[AnalysisV3, AnalysisV4]
+AnalysisStorageType = Union[AnalysisStorageV3, AnalysisStorageV4]
+ProjectPipelineType = Union[ProjectPipeline, ProjectPipelineV4]
+
+# Class globals
+CES_WORKING_DIR_PLACEHOLDER = "__CES_WORKING_DIR__"
+CES_WORKING_DIR_REPLACEMENT = "/ces/scheduler/run/{__ANALYSIS_ID__}"
+CES_DATA_ABS_PATH = Path("__CES_WORKING_DIR__") / "data"
+SAMPLESHEET_WITH_PLACEHOLDERS_NAME = ".samplesheet_with_placeholders.csv.tmp"
+SAMPLESHEET_WITH_ABS_PATHS_NAME = "samplesheet_nf.csv"
+SAMPLESHEET_DIR_NAME = "wrapica_samplesheet_dir"
 
 # Import everything
 from .functions.project_pipelines_functions import (
@@ -89,19 +103,6 @@ from .classes.nextflow_analysis import (
     ICAv2NextflowEngineParameters,
     ICAv2NextflowPipelineAnalysis
 )
-
-# Literal / Types
-AnalysisType = Union[AnalysisV3, AnalysisV4]
-AnalysisStorageType = Union[AnalysisStorageV3, AnalysisStorageV4]
-ProjectPipelineType = Union[ProjectPipeline, ProjectPipelineV4]
-
-# Class globals
-CES_WORKING_DIR_PLACEHOLDER = "__CES_WORKING_DIR__"
-CES_WORKING_DIR_REPLACEMENT = "/ces/scheduler/run/{__ANALYSIS_ID__}"
-CES_DATA_ABS_PATH = Path("__CES_WORKING_DIR__") / "data"
-SAMPLESHEET_WITH_PLACEHOLDERS_NAME = ".samplesheet_with_placeholders.csv.tmp"
-SAMPLESHEET_WITH_ABS_PATHS_NAME = "samplesheet_nf.csv"
-SAMPLESHEET_DIR_NAME = "wrapica_samplesheet_dir"
 
 __all__ = [
     # Libica models
